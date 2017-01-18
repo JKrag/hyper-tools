cc5a71bf8bcde6c3675146148a7c47ae3c0983bf
Jan Krag <jankrag@gmail.com>
2017-01-18 15:19:26 +0100

    Make output less noisy when no 'things'  are found

--- hyper-list.sh
+++ hyper-list.sh
@@ -1,17 +1,49 @@
 echo "--- Containers ------------------------------------------------------"
-hyper ps -a
+cont=`hyper ps -a`
+if [`wc -l <<< "$cont"` -gt 1 ]; then
+	echo "$cont"
+fi
+
 echo "--- Volumes ---------------------------------------------------------"
-hyper volume ls
+vol=`hyper volume ls`
+if [`wc -l <<< "$vol"` -gt 1 ]; then
+	echo "$vol"
+fi
+
 echo "--- Images ----------------------------------------------------------"
-hyper images
+img=`hyper images`
+if [`wc -l <<< "$img"` -gt 1 ]; then
+	echo "$img"
+fi
+
 echo "--- FIP'S -----------------------------------------------------------"
-hyper fip ls
+fips=`hyper fip ls`
+if [`wc -l <<< "$fips"` -gt 1 ]; then
+	echo "$fips"
+fi
+
+
 echo "--- Services --------------------------------------------------------"
-hyper service ls
+srv=`hyper service ls`
+if [`wc -l <<< "$srv"` -gt 1 ]; then
+	echo "$srv"
+fi
+
 echo "--- Cron ------------------------------------------------------------"
-hyper cron ls
+crn=`hyper cron ls`
+if [`wc -l <<< "$crn"` -gt 1 ]; then
+	echo "$crn"
+fi
+
 echo "--- Security groups -------------------------------------------------"
-hyper sg ls
+sgs=`hyper sg ls`
+if [`wc -l <<< "$sgs"` -gt 1 ]; then
+	echo "$sgs"
+fi
+
+
 echo "--- Snapshots -------------------------------------------------------"
-hyper snapshot ls
-echo "---------------------------------------------------------------------"
+snap=`hyper snapshot ls`
+if [`wc -l <<< "$snap"` -gt 1 ]; then
+	echo "$snap"
+fi
